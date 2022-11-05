1cef227ee53621cc7735e8ffe599980e5ee04348
Steven Fabre <hello@stevenfabre.com>
2022-11-05 18:46:08 -0400

    Implemented tailwind + custom font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import "../styles/globals.css";
+import "../src/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Familjen_Grotesk } from "@next/font/google";
+
+const font = Familjen_Grotesk();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={font.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const { spacing, fontFamily } = require('tailwindcss/defaultTheme');
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Familjen Grotesk', ...fontFamily.sans]
+      }
+    },
+  },
+  plugins: [],
+}

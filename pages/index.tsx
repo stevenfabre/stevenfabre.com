e4f21a884ef13c7dc70fc98b4efcf28eca156a3d
Steven Fabre <hello@stevenfabre.com>
2022-11-05 09:10:45 -0400

    Using proper favicon and formating

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -8,7 +7,7 @@ export default function Home() {
       <Head>
         <title>Steven Fabre</title>
         <meta name="description" content="CEO of Liveblocks" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
 
       <main className={styles.main}>

8272f8a1a166996526bffe2bf21b39c1fdfb0086
Steven Fabre <hello@stevenfabre.com>
2022-11-06 16:40:32 -0500

    Added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../src/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Familjen_Grotesk } from "@next/font/google";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
 
 const font = Familjen_Grotesk();
 
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <ThemeProvider attribute="class">
       <main className={font.className}>
         <Component {...pageProps} />
+        <Analytics />
       </main>
     </ThemeProvider>
   );

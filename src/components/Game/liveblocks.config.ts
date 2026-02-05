f3430f4fd91b99e1f86759bd8d190db4881db4a8
Steven Fabre <hello@stevenfabre.com>
2026-02-05 13:23:22 +0100

    Multitouch game

--- src/components/Game/liveblocks.config.ts
+++ src/components/Game/liveblocks.config.ts
@@ -2,7 +2,7 @@ import { LiveList, LiveObject } from "@liveblocks/client";
 import { CursorType, Pixel, Point } from "./types";
 
 export type Presence = {
-  cursor: Point | null;
+  cursor: Point[] | null;
   color: string;
   cursorType: CursorType | null;
 };
